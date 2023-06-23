feb591f759f79312aa06b0be5d89b22c59149d0c
Matt Goetz <mattgoetz6@gmail.com>
2023-06-23 08:59:30 -0400

    added a loop to check the operations table for links

--- content.js
+++ content.js
@@ -9,6 +9,7 @@ const startingOperationIndex = document.getElementById('indx_startoperation1');
 const endingOperationIndex = document.getElementById('indx_endoperation2');
 const startingOperationHidden = document.getElementById('hddn_startoperation1');
 const endingOperationHidden = document.getElementById('hddn_endoperation2');
+const completedQtyActual = document.getElementById('completedquantity');
 
 const operationrow0 = document.getElementById('operationrow0');
 const operationrow1 = document.getElementById('operationrow1');
@@ -24,30 +25,97 @@ const operationrow10 = document.getElementById('operationrow10');
 
 const operationTable = document.getElementById('operation_splits');
 const compTable = document.getElementById('component_splits');
+
 const rows = compTable.getElementsByTagName('tr');
 const opRows = operationTable.getElementsByTagName('tr');
 
 let endOp = opRows.length - 1;
-let netsuiteNumber = 17963256;
+let netsuiteNumber = 17963256; //startoperation is 17986474 which equates to 1
+let startNetsuiteNumber = 17986474;
 
-completedQty.value = qty.innerHTML;
+const eventChange = new Event('change', { bubbles: true });
+const eventKeyDown = new Event('keydown', { bubbles: true });
+const eventKeyUp = new Event('keyup', { bubbles: true });
+const eventKeyPress = new Event('keypress', { bubbles: true });
+const eventFocus = new Event('focus', { bubbles: true });
+const eventMouseDown = new Event('mousedown', { bubbles: true })
+const eventBlur = new Event('blur', { bubbles: true })
+
+//alert(qty.innerHTML);
+//alert(qty.innerText);
+
 startingOperation.title = 1;
 startingOperation.value = 1;
+startingOperationIndex.value = 1;
+startingOperationHidden.value = startNetsuiteNumber;
+
+startingOperation.dispatchEvent(eventMouseDown);
+startingOperation.dispatchEvent(eventFocus);
+startingOperation.dispatchEvent(eventKeyDown);
+startingOperation.dispatchEvent(eventKeyPress);
+startingOperation.dispatchEvent(eventChange);
+startingOperation.dispatchEvent(eventBlur);
+
 endingOperation.title = endOp;
 endingOperation.value = endOp;
-
-startingOperationIndex.value = 1;
 endingOperationIndex.value = endOp;
+endingOperationHidden.value = startNetsuiteNumber + (endOp - 1);
 
-startingOperationHidden.value = netsuiteNumber;
-endingOperationHidden.value = netsuiteNumber + (endOp - 1);
+endingOperation.dispatchEvent(eventMouseDown);
+endingOperation.dispatchEvent(eventFocus);
+endingOperation.dispatchEvent(eventKeyDown);
+endingOperation.dispatchEvent(eventKeyPress);
+endingOperation.dispatchEvent(eventChange);
+endingOperation.dispatchEvent(eventBlur);
 
-for (let i = 1; i < rows.length; i++) {
-    const currentRow = rows[i];
+
+try {
+
+    if (qty.innerText === null) {
+        qty.innerText = 1;
+    }
+    completedQty.value = qty.innerText;
+    completedQtyActual.value = qty.innerText;
+} catch (error) {
+    qty.innerText = 1;
+    completedQty.value = 1;
+    completedQtyActual.value = 1;
+}
+
+completedQty.dispatchEvent(eventKeyUp);
+completedQty.dispatchEvent(eventMouseDown);
+completedQty.dispatchEvent(eventFocus);
+completedQty.dispatchEvent(eventKeyPress);
+completedQty.dispatchEvent(eventKeyUp);
+completedQty.dispatchEvent(eventChange);
+completedQty.dispatchEvent(eventBlur);
+completedQty.dispatchEvent(eventChange);
+
+completedQtyActual.dispatchEvent(eventKeyUp);
+completedQtyActual.dispatchEvent(eventMouseDown);
+completedQtyActual.dispatchEvent(eventFocus);
+completedQtyActual.dispatchEvent(eventKeyPress);
+completedQtyActual.dispatchEvent(eventKeyUp);
+completedQtyActual.dispatchEvent(eventChange);
+completedQtyActual.dispatchEvent(eventBlur);
+completedQtyActual.dispatchEvent(eventChange);
+
+
+for (let i = 1; i < opRows.length; i++) {
+    const currentRow = opRows[i];
     const cells = currentRow.getElementsByTagName('td');
 
+    if (cells[2].innerHTML.includes('/app/accounting') {
+        alert("test");
+    }
+
+    /*
     const thirdColumnValue = cells[2].innerText;
     const fourthColumnField = document.getElementById('quantity' + i);
     //cells[3].innerText = thirdColumnValue * qty.innerHTML;
     fourthColumnField.value = thirdColumnValue * qty.innerHTML;
+    */
 }
+
+
+
