64446292b2ed45ba44dcd309b2cd8c76a5d1ffb6
Matt Goetz <mattgoetz6@gmail.com>
2023-06-23 12:03:57 -0400

    THIS WORKS NOW!! netsuite does some real fuckery let me tell ya

--- content.js
+++ content.js
@@ -3,66 +3,90 @@ const qty = document.getElementById('orderquantity_val');
 const qtyToBuild = document.getElementById('quantity');
 const qtyInput = document.getElementById('quantity_formattedValue');
 const completedQty = document.getElementById('completedquantity_formattedValue');
+
+/*
+ * So, the next 6 lines have a weird quirk. See how they are all labeled with a number?
+ * Well on my machine, the fields are called startoperation1 and endoperation2
+ * But on a different machine, such as dennis' pc, the fields are called startoperation2 and endoperation2
+ * We need to write a function that detects which fields are actually being used and changes them all accordingly
+ */
 const startingOperation = document.getElementById('inpt_startoperation1');
 const endingOperation = document.getElementById('inpt_endoperation2');
 const startingOperationIndex = document.getElementById('indx_startoperation1');
 const endingOperationIndex = document.getElementById('indx_endoperation2');
 const startingOperationHidden = document.getElementById('hddn_startoperation1');
 const endingOperationHidden = document.getElementById('hddn_endoperation2');
-const completedQtyActual = document.getElementById('completedquantity');
 
-const operationrow0 = document.getElementById('operationrow0');
-const operationrow1 = document.getElementById('operationrow1');
-const operationrow2 = document.getElementById('operationrow2');
-const operationrow3 = document.getElementById('operationrow3');
-const operationrow4 = document.getElementById('operationrow4');
-const operationrow5 = document.getElementById('operationrow5');
-const operationrow6 = document.getElementById('operationrow6');
-const operationrow7 = document.getElementById('operationrow7');
-const operationrow8 = document.getElementById('operationrow8');
-const operationrow9 = document.getElementById('operationrow9');
-const operationrow10 = document.getElementById('operationrow10');
+const completedQtyActual = document.getElementById('completedquantity');
 
 const operationTable = document.getElementById('operation_splits');
 const compTable = document.getElementById('component_splits');
+const divs = document.getElementsByTagName('div');
 
 const rows = compTable.getElementsByTagName('tr');
 const opRows = operationTable.getElementsByTagName('tr');
 
 let endOp = opRows.length - 1;
-let netsuiteNumber = 17963256; //startoperation is 17986474 which equates to 1
+
+/*the magic netsuite numbers.... we need these to be correct each time for starting an end operation, but they vary from form to form.
+ *What we need to do is search all divs for any div with the attribute: class "ns-dropdown" and the attribute: data-name "startoperation"
+ *this div will have an attribute called data-options that contians an array with values to represent 1-whatever
+ * i am here to find this number.
+ */
+
 let startNetsuiteNumber = 17986474;
 
-const eventChange = new Event('change', { bubbles: true });
-const eventKeyDown = new Event('keydown', { bubbles: true });
-const eventKeyUp = new Event('keyup', { bubbles: true });
-const eventKeyPress = new Event('keypress', { bubbles: true });
-const eventFocus = new Event('focus', { bubbles: true });
-const eventMouseDown = new Event('mousedown', { bubbles: true })
-const eventBlur = new Event('blur', { bubbles: true })
+for (let i = 0; i < divs.length; i++) {
+    if (divs[i].getAttribute('class') === "ns-dropdown" && divs[i].getAttribute('data-name') === "startoperation") {
+        const valueArray = divs[i].getAttribute('data-options');
+        const optionsArray = JSON.parse(valueArray);
 
-//alert(qty.innerHTML);
-//alert(qty.innerText);
+        for (const option of optionsArray) {
+            if (option.text === '1') {
+                startNetsuiteNumber = option.value;
+                break;
+            }
+        }
+    }
+}
+
+//netsuite needs events. lets give it to em
+const eventChange = new Event('change', { bubbles: true });
+const eventKeyDown = new KeyboardEvent('keydown', { bubbles: true });
+const eventKeyUp = new KeyboardEvent('keyup', { bubbles: true });
+const eventKeyPress = new KeyboardEvent('keypress', { bubbles: true });
+const eventFocus = new FocusEvent('focus', { bubbles: true });
+const eventMouseDown = new MouseEvent('mousedown', { bubbles: true });
+const eventBlur = new FocusEvent('blur', { bubbles: true });
+const eventInput = new InputEvent('input', { bubbles: true });
+
+//starting operation
+startingOperation.dispatchEvent(eventFocus);
+startingOperation.dispatchEvent(eventKeyPress);
+startingOperation.dispatchEvent(eventInput);
 
 startingOperation.title = 1;
 startingOperation.value = 1;
 startingOperationIndex.value = 1;
 startingOperationHidden.value = startNetsuiteNumber;
 
-startingOperation.dispatchEvent(eventMouseDown);
-startingOperation.dispatchEvent(eventFocus);
+startingOperation.dispatchEvent(eventInput);
 startingOperation.dispatchEvent(eventKeyDown);
 startingOperation.dispatchEvent(eventKeyPress);
 startingOperation.dispatchEvent(eventChange);
 startingOperation.dispatchEvent(eventBlur);
 
+//ending operation
+endingOperation.dispatchEvent(eventFocus);
+endingOperation.dispatchEvent(eventKeyPress);
+endingOperation.dispatchEvent(eventInput);
+
 endingOperation.title = endOp;
 endingOperation.value = endOp;
 endingOperationIndex.value = endOp;
-endingOperationHidden.value = startNetsuiteNumber + (endOp - 1);
+endingOperationHidden.value = parseInt(startNetsuiteNumber) + (endOp - 1);
 
-endingOperation.dispatchEvent(eventMouseDown);
-endingOperation.dispatchEvent(eventFocus);
+endingOperation.dispatchEvent(eventInput);
 endingOperation.dispatchEvent(eventKeyDown);
 endingOperation.dispatchEvent(eventKeyPress);
 endingOperation.dispatchEvent(eventChange);
@@ -82,40 +106,18 @@ try {
     completedQtyActual.value = 1;
 }
 
+completedQty.dispatchEvent(eventFocus);
 completedQty.dispatchEvent(eventKeyUp);
 completedQty.dispatchEvent(eventMouseDown);
-completedQty.dispatchEvent(eventFocus);
 completedQty.dispatchEvent(eventKeyPress);
 completedQty.dispatchEvent(eventKeyUp);
 completedQty.dispatchEvent(eventChange);
-completedQty.dispatchEvent(eventBlur);
 completedQty.dispatchEvent(eventChange);
+completedQty.dispatchEvent(eventBlur);
 
-completedQtyActual.dispatchEvent(eventKeyUp);
-completedQtyActual.dispatchEvent(eventMouseDown);
 completedQtyActual.dispatchEvent(eventFocus);
-completedQtyActual.dispatchEvent(eventKeyPress);
-completedQtyActual.dispatchEvent(eventKeyUp);
 completedQtyActual.dispatchEvent(eventChange);
 completedQtyActual.dispatchEvent(eventBlur);
-completedQtyActual.dispatchEvent(eventChange);
-
-
-for (let i = 1; i < opRows.length; i++) {
-    const currentRow = opRows[i];
-    const cells = currentRow.getElementsByTagName('td');
-
-    if (cells[2].innerHTML.includes('/app/accounting') {
-        alert("test");
-    }
-
-    /*
-    const thirdColumnValue = cells[2].innerText;
-    const fourthColumnField = document.getElementById('quantity' + i);
-    //cells[3].innerText = thirdColumnValue * qty.innerHTML;
-    fourthColumnField.value = thirdColumnValue * qty.innerHTML;
-    */
-}
 
 
 
